9f91f7a1af70b7ad87abac35d2499da0d29d9e00
ThanhMai6507 <main6507@gmail.com>
2023-03-03 16:03:03 +0700

    bai4 fix

--- Bai4/form.php
+++ Bai4/form.php
@@ -1,15 +1,45 @@
-<?php
-    $question = explode("\n", file_get_contents("question.txt")); 
-    $ans = explode("\n", file_get_contents("answer.txt")); 
-    $answer = array_chunk($ans, 4);
-    $select_answer = []; // lưu đáp án được chọn
-    $result = explode("\n", file_get_contents("question.txt")); // đáp án đúng
-    $count = 0;
+<?php 
+    session_start();
+
+    $question = [];
+    $quests = ['question1.txt', 'question2.txt'];
+    foreach ($quests as $file) {
+        $content = file_get_contents($file);
+        $question[] = $content;
+    }
+
+    $answer = [];
+    $ans = ['answer1.txt', 'answer2.txt'];
+    foreach ($ans as $file) {
+        $content = explode("\n",file_get_contents($file));
+        $answer[] = $content;
+    }
+
+    $correct_answer = [];
+    $true_ans = ['correct_answer1.txt', 'correct_answer2.txt'];
+    foreach ($true_ans as $file) {
+        $content = file_get_contents($file);
+        $correct_answer[] = $content;
+    }
+
+    // print_r($question);
+    // print_r($answer);
+
+    // $select_answer = []; // lưu đáp án được chọn
+    // $result = explode("\n", file_get_contents("correct_answer.txt")); // đáp án đúng
+    // $count = 0;
+
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     $_SESSION = $_POST;
+    //     header("Location: submit.php");
+    // }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $_SESSION = $_POST;
+        $_SESSION["answer"] = $answer;
+        $_SESSION["question"] = $question;
         header("Location: submit.php");
     }
+
 ?>
 
 <form method="POST">
@@ -19,24 +49,16 @@
             echo "<p>Câu {$key}: {$value}</p>";
             foreach ($answer as $key_ans => $value_ans) {
                 if ($key_ans == $key) {
-                    foreach ($value_ans as $val => $dapan) {
+                    foreach ($value_ans as $vlue  => $dan) {
                         // lấy đáp án được chọn
-                        if (isset($_POST[$val])){
-                            $select_answer[$val] = $dapan;
-                        }
-                        echo "<input type=\"radio\" name=\"question_{$key_ans}\" value=\"{$val}\"> {$dapan}<br>";
+                        // if (isset($_POST[$val])){
+                        //     $select_answer[$val] = $dan;
+                        // }
+                        echo "<input type=\"radio\" name=\"question_{$key_ans}\" value=\"{$vlue }\"> {$dan}<br>";
                     }
                 }
             }
         }
-        if (isset($_POST['submit'])) {
-            foreach($select_answer as $key=>$answer) {
-                if ($answer == $result[$key]){
-                    $count++;
-                }
-            }
-            return $count;
-        }
-        
-        print_r($select_answer);
+
+        // print_r($select_answer);
     ?>
--- Bai4/quizz.php
+++ Bai4/quizz.php
@@ -7,9 +7,63 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        include('form.php');
+<?php 
+    session_start();
+
+    // $question = [];
+    // $quests = ['question1.txt', 'question2.txt'];
+    // foreach ($quests as $file) {
+    //     $content = file_get_contents($file);
+    //     $question[] = $content;
+    // }
+
+    // $answer = [];
+    // $ans = ['answer1.txt', 'answer2.txt'];
+    // foreach ($ans as $file) {
+    //     $content = explode("\n", file_get_contents($file));
+    //     $answer[] = $content;
+    // }
+
+    // $correct_answer = [];
+    // $true_ans = ['correct_answer1.txt', 'correct_answer2.txt'];
+    // foreach ($true_ans as $file) {
+    //     $content = explode("\n", file_get_contents($file));
+    //     $correct_answer[] = $content;
+    // }
+    // $a = array_values($correct_answer);
+
+    // print_r($a);
+
+    $question = array(1 => "Vừa gà vừa chó, bó lại cho tròn, ba mươi sáu con, một trăm chân chẵn, hỏi có mấy gà, mấy chó", 
+                      2 => "Rồng đất là con gì");
+
+    $answer = array(1 => array("100 gà, 100 chó" => 0, "10 gà, 10 chó" => 1, "20 gà, 20 chó" => 0, "5 gà, 5 chó" => 0), 
+                    2 => array("gà" => 0, "dế" => 0, "voi" => 0, "giun" => 1));
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $_SESSION["answer"] = $answer;
+        $_SESSION["question"] = $question;
+        header("Location: submit.php");
+    }
+
+?>
+
+<form method="POST">
+    <h1>Trắc nghiệm thông minh</h1>
+    <?php
+        foreach ($question as $key => $value) {
+            echo "<p>Câu {$key}: {$value}</p>";
+            foreach ($answer as $key_ans => $value_ans) {
+                if ($key == $key_ans){
+                    foreach ($value_ans as $vlue => $dan) {
+                        echo "<input type=\"radio\" name=\"question_{$key_ans}\" value=\"{$vlue}\"> {$vlue}<br>";
+                    }
+                }
+            }
+        };
+
     ?>
     <button name="submit">Submit</button>
+</form>
 </body>
 </html>
